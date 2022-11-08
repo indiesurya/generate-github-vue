efde68928ff7bd12a17669ff052c997aa632d9b4
indiesurya <putuindie11@gmail.com>
2022-11-09 01:12:37 +0800

    Update setup app

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
-
+import Home from "./views/pages/HomeView.vue";
+import Navbar from "./views/components/NavbarView.vue";
 </script>
 
 <template>
-  
+  <Navbar/>
+  <router-view/>
 </template>
 
 <style scoped>
