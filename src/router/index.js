13ad87af6a1e6685d40e52668f191ce015e7fd91
indiesurya <putuindie11@gmail.com>
2022-11-09 01:12:49 +0800

    Add routing

---
+++ src/router/index.js
@@ -0,0 +1,23 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from '../views/pages/HomeView.vue'
+import About from '../views/pages/AboutView.vue'
+
+const routes = [
+    {
+        path: '/',
+        name: 'home',
+        component: Home
+    },
+    {
+        path: '/about',
+        name: 'about',
+        component: About
+    },
+]
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+})
+
+export default router
